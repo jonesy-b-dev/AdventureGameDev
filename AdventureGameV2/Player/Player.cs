f23eb1b7560c9881917b5d29bae24c5817ff5bf9
Jonas de Bruin <jonasdebruin05@gmail.com>
2022-12-04 16:05:32 +0100

    Update to namespaces

--- AdventureGameV2/Player/Player.cs
+++ AdventureGameV2/Player/Player.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventureGame
+namespace AdventureGameV2
 {
     internal class Player
     {
--- AdventureGameV2/Source.cs
+++ AdventureGameV2/Source.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventureGame
+namespace AdventureGameV2
 {
     internal class Source
     {
